0c74cbb11750cc6b2cbb32b323075d0c2ce24663
ckcz123 <ckcz123@126.com>
2017-04-21 21:12:09 +0800

    Codejam 2016 Round 3

---
+++ codejam/2016/Round3/A.java
@@ -0,0 +1,36 @@
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+    private String solve(Scanner scanner) {
+        StringBuilder builder=new StringBuilder(scanner.next());
+        int cnt=0;
+        while (true) {
+            boolean has=false;
+            for (int i=0;i<builder.length()-1;i++) {
+                if (builder.charAt(i)==builder.charAt(i+1)) {
+                    builder.replace(i, i+2, "");
+                    cnt+=10;
+                    has=true;
+                    break;
+                }
+            }
+            if (!has) break;
+        }
+        return String.valueOf(cnt+builder.length()*5/2);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream("output.txt"));
+        Scanner scanner=new Scanner(System.in);
+        int times=scanner.nextInt();
+        long start=System.currentTimeMillis();
+        for (int t=1;t<=times;t++) {
+            System.out.println(String.format("Case #%d: %s", t, new Main().solve(scanner)));
+        }
+        long end=System.currentTimeMillis();
+        System.err.println(String.format("Time used: %.3fs", (end-start)/1000.0));
+    }
+
+}
---
+++ codejam/2016/Round3/D.java
@@ -0,0 +1,35 @@
+import java.io.PrintStream;
+import java.util.*;
+
+public class Main {
+
+    private String solve(Scanner scanner) {
+        int n=scanner.nextInt(), l=scanner.nextInt();
+        HashSet<String> set=new HashSet<>();
+        for (int i=0;i<n;i++) set.add(scanner.next());
+        String s=scanner.next();
+        if (set.contains(s)) return "IMPOSSIBLE";
+        if (l==1) {
+            if ("0".equals(s)) return "1 1?";
+            return "0 0?";
+        }
+        String x=s.replace('0','A').replace('1','B')
+                .replace("A","1?").replace("B","0?");
+        String y=s.substring(0, l-1).replace('0','A').replace('1','B')
+                .replace("A","10").replace("B","01");
+        return x+" "+y;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setOut(new PrintStream("output.txt"));
+        Scanner scanner=new Scanner(System.in);
+        int times=scanner.nextInt();
+        long start=System.currentTimeMillis();
+        for (int t=1;t<=times;t++) {
+            System.out.println(String.format("Case #%d: %s", t, new Main().solve(scanner)));
+        }
+        long end=System.currentTimeMillis();
+        System.err.println(String.format("Time used: %.3fs", (end-start)/1000.0));
+    }
+
+}
