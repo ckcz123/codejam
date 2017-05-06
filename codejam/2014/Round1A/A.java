561919203dd3a753d163372661d3ca77dcc86a6b
ckcz123 <ckcz123@126.com>
2017-05-06 16:54:57 +0800

    Codejam 2014 Round 1A

--- codejam/2014/Round1A/A.java
+++ codejam/2014/Round1A/A.java
@@ -2,6 +2,10 @@ import java.io.PrintStream;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Codejam 2014 Round 1A
+ * Problem A. Charging Chaos
+ */
 public class Main {
 
     private String solve(Scanner scanner) {
