3eabe3a9f3835fe21182b59591db93ffc0c50f67
ckcz123 <ckcz123@126.com>
2018-03-10 12:49:39 +0800

    Update 2016 Round 1A

--- codejam/2016/Round1A/C.java
+++ codejam/2016/Round1A/C.java
@@ -8,53 +8,6 @@ import java.util.stream.Collectors;
  */
 public class Main {
 
-    private String solve(Scanner scanner) {
-        int n=scanner.nextInt();
-        int[] f=new int[n];
-        ArrayList<Integer>[] lists=new ArrayList[n];
-        for (int i=0;i<n;i++) {
-            lists[i]=new ArrayList<>();
-        }
-        for (int i=0;i<n;i++) {
-            int x=scanner.nextInt()-1;
-            f[i]=x;
-            lists[x].add(i);
-        }
-        int ans=0;
-        for (int i=0;i<n;i++)
-            ans=Math.max(ans, calCycleLength(i, f));
-        PriorityQueue<Integer> priorityQueue=new PriorityQueue<>((o1,o2)->o2-o1);
-        for (int i=0;i<n;i++)
-            if (f[f[i]]==i && f[i]<i)
-                priorityQueue.add(calBiLength(i, f[i], lists));
-        ans=Math.max(ans, priorityQueue.stream().mapToInt(i->i).sum());
-        return String.valueOf(ans);
-    }
-
-    private int calCycleLength(int s, int[] f) {
-        ArrayList<Integer> arrayList=new ArrayList<>();
-        while (true) {
-            int index=arrayList.indexOf(s);
-            if (index!=-1)
-                return arrayList.size()-index;
-            arrayList.add(s);
-            s=f[s];
-        }
-    }
-
-    private int calBiLength(int s, int e, ArrayList<Integer>[] lists) {
-        return depth(s, e, lists)+depth(e, s, lists);
-    }
-
-    private int depth(int s, int e, ArrayList<Integer>[] lists) {
-        int x=1;
-        for (int y: lists[s]) {
-            if (y!=e)
-                x=Math.max(x, 1+depth(y, s, lists));
-        }
-        return x;
-    }
-
     public static void main(String[] args) throws Exception {
         System.setOut(new PrintStream("output.txt"));
         Scanner scanner=new Scanner(System.in);
@@ -65,7 +18,53 @@ public class Main {
         }
         long end=System.currentTimeMillis();
         System.err.println(String.format("Time used: %.3fs", (end-start)/1000.0));
+    }
 
+    public String solve(Scanner scanner) {
+        int n=scanner.nextInt();
+        int[] f=new int[n];
+        ArrayList<Integer>[] children=new ArrayList[n];
+        for (int i=0;i<n;i++) children[i]=new ArrayList<>();
+        for (int i=0;i<n;i++) {
+            int x=scanner.nextInt()-1;
+            f[i]=x;
+            children[x].add(i);
+        }
+
+        int ans=0;
+        // Step 1: find circle
+        for (int i=0;i<n;i++)
+            ans=Math.max(ans, calCircle(n, i, f));
+
+        // Step 2: find bi-linked
+        int v=0;
+        for (int i=0;i<n;i++) {
+            if (f[f[i]]==i) {
+                v+=calDepth(i, f[i], children);
+            }
+        }
+        return String.valueOf(Math.max(ans, v));
+    }
+
+    private int calCircle(int n, int x, int[] f) {
+        boolean[] visited=new boolean[n];
+        visited[x]=true;
+        int len=1, curr=f[x];
+        while (curr!=x) {
+            if (visited[curr]) return 0;
+            visited[curr]=true;
+            len++;
+            curr=f[curr];
+        }
+        return len;
+    }
+
+    private int calDepth(int x, int not, ArrayList<Integer>[] children) {
+        int ans=0;
+        for (int w: children[x])
+            if (w!=not)
+                ans=Math.max(ans, calDepth(w, not, children));
+        return ans+1;
     }
 
 }
